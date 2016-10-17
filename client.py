8f52683900615c6c2c8f13192eaf6b9129e2e725
Cristian Pilco Miranda <cdpilco@gamma18.aulas.gsyc.urjc.es>
2016-10-17 10:13:49 +0200

    modificado el client para recibir parametros por consola

--- client.py
+++ client.py
@@ -5,18 +5,23 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+SERVER = sys.argv[1]
+PORT = int(sys.argv[2]) 
+LINE = ' '.join(sys.argv[3:]) 
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+# tipo de red, tipo de paquete son constantes de paquete socket
+# socket.AF_INET : camino de internet
+# socket.SOCK_DGRAM: utilizamos UDP
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((SERVER, PORT)) # nos conectamos con el servidor tupla
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')# enviamos 
+    # cadena de caracteres en bytes 
+    data = my_socket.recv(1024) # guarda en data lo q recibo tamaño buffer
+    print('Recibido -- ', data.decode('utf-8'))# decodifica de bytes a utf-8
+#cerramos el with
 print("Socket terminado.")
