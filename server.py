f5ac4784c778869d4ae9ee9b773e1bcdb14382d0
Cristian Pilco Miranda <cdpilco@gamma18.aulas.gsyc.urjc.es>
2016-10-17 10:47:22 +0200

    imprimi ip y puerto cliente y puerto server como parametro

--- server.py
+++ server.py
@@ -5,22 +5,32 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
+# socketserver.DatagramRequestHandler : heredamos para manejar UDP
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
 
-    def handle(self):
+    def handle(self): # TRATAMOS EL SOCKET COMO UN FICHERO
+        Ip_client = self.client_address[0]
+        P_client = self.client_address[1]
+        
         self.wfile.write(b"Hemos recibido tu peticion")
-        for line in self.rfile:
+        for line in self.rfile:# leemos el socket
+            print("IP cliente = ", Ip_client, "Puerto cliente = ", P_client)
             print("El cliente nos manda ", line.decode('utf-8'))
+            
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+
+    # ip y puerto donde escucha el servidor y clase que maneja la peticion
+    PORT = int(sys.argv[1]) 
+    serv = socketserver.UDPServer(('', PORT), EchoHandler) 
     print("Lanzando servidor UDP de eco...")
     try:
+        # bucle esperando peticiones
         serv.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
