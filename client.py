9b6b2e93967babd55709736af356d18c7a987caa
cristpm <crisdario21@gmail.com>
2016-10-23 23:18:57 +0200

    pep8

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 try:
     SERVER = sys.argv[1]
-    PORT = int(sys.argv[2]) 
+    PORT = int(sys.argv[2])
     USER = sys.argv[4]
     Expires = sys.argv[5]
     LINE = 'REGISTER sip:' + USER + ' SIP/2.0\r\nExpires: ' + Expires + '\r\n'
@@ -18,16 +18,11 @@ except IndexError:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-# tipo de red, tipo de paquete son constantes de paquete socket
-# socket.AF_INET : camino de internet
-# socket.SOCK_DGRAM: utilizamos UDP
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT)) # nos conectamos con el servidor tupla
+    my_socket.connect((SERVER, PORT))  # nos conectamos con el servidor tupla
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')# enviamos 
-    # cadena de caracteres en bytes 
-    data = my_socket.recv(1024) # guarda en data lo q recibo tamaño buffer
-    print('Recibido -- ', data.decode('utf-8'))# decodifica de bytes a utf-8
-#cerramos el with
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')  # enviamos
+    # cadena de caracteres en bytes
+    data = my_socket.recv(1024)  # guarda en data lo q recibo tamaño buffer
+    print('Recibido -- ', data.decode('utf-8'))  # decodifica de bytes a utf-8
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -10,28 +10,29 @@ import time
 import json
 # socketserver.DatagramRequestHandler : heredamos para manejar UDP
 
+
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     clientes = []
 
-    def handle(self): # TRATAMOS EL SOCKET COMO UN FICHERO
-    
+    def handle(self):  # TRATAMOS EL SOCKET COMO UN FICHERO
+
         self.json2registered()
         self.expiration()
         Ip_client = self.client_address[0]
         P_client = self.client_address[1]
         print('TUS DATOS SON:', "IP = ", Ip_client, "Puerto = ", P_client)
-        for line in self.rfile:# leemos el socket
+        for line in self.rfile:  # leemos el socket
             l = line.decode('utf-8')
-            ##print(l)
+            # print(l)
             if l.split(' ')[0] == 'REGISTER':
                 direc = l.split(' ')[1][4:]
             if l.split(' ')[0] == 'Expires:':
                 t = int(l.split(' ')[1][0:-2])
         exp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() + t))
-        client = [direc, {"address": Ip_client , "expires" : exp}] 
+        client = [direc, {"address": Ip_client, "expires": exp}]
         if t == 0:
             for user in self.clientes:
                 if user[0] == direc:
@@ -41,30 +42,29 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         self.register2json()
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         print("CLIENTES ==> ", self.clientes)
-        
+
     def register2json(self):
-        json.dump(self.clientes, open('registered.json', 'w'),indent = 4)
-  
+        json.dump(self.clientes, open('registered.json', 'w'), indent=4)
+
     def expiration(self):
         gmt = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
         for user in self.clientes:
             if gmt > user[1]['expires']:
                 self.clientes.remove(user)
-                
+
     def json2registered(self):
         try:
-            with open('registered.json') as data_file:    
+            with open('registered.json') as data_file:
                 self.clientes = json.load(data_file)
         except:
-            print('NO EXISTE registered.json') 
-            
-            
-            
+            print('NO EXISTE registered.json')
+
+
 if __name__ == "__main__":
 
     # ip y puerto donde escucha el servidor y clase que maneja la peticion
-    PORT = int(sys.argv[1]) 
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
+    PORT = int(sys.argv[1])
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         # bucle esperando peticiones
